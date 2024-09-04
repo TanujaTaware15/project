014d73dc8579d0099188a4d52788ef3d74431ad5
TanujaTaware15 <tawaretanuja0@gmail.com>
2024-09-04 17:28:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+gsap.from('.header',{
+    opacity:0,
+    y:-25,
+    delay:0.5,
+    duration:0.5
+})
+gsap.from('.header h2',{
+    opacity:0,
+    x:-120,
+    delay:1,
+    duration:0.5
+
+})
+gsap.from('.header .links a',{
+    opacity:0,
+    y:-20,
+    delay:1.5,
+    duration:0.5,
+    stagger:0.6
+
+})
+gsap.from('.header .links button',{
+    opacity:0,
+    x:250,
+    delay:3.9,
+    duration:0.5
+
+})
