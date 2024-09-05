b780c6d78e3e944894066f4c84031be0bdc3ccf2
Ankita Patil <ankitavp26@gmail.com>
2024-09-05 18:33:48 +0530

    feature/ankita

--- script.js
+++ script.js
@@ -26,3 +26,71 @@ gsap.from('.header .links button',{
     duration:0.5
 
 })
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>=450){
+        tl1.restart()
+    }
+})
+
+var tl1=gsap.timeline()
+
+tl1.from('.container1 h1',{
+    opacity:0,
+    x:100,
+    scrollTrigger:{
+        trigger:'.container1 .title h1',
+        scroller:'body',
+        start:'top 50%',
+        end:'top 50%',
+        scrub:5
+    }
+})
+tl1.from('.container1 .skeleton .info h3',{
+    opacity:0,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:'.container1 .skeleton .info h3',
+        scroller:'body',
+        start:'top 60%',
+        end:'top 60%',
+        scrub:2
+    }
+})
+tl1.from('.container1 .skeleton p',{
+    opacity:0,
+    y:70,
+    stagger:0.2,
+    scrollTrigger:{
+        trigger:'.container1 .skeleton p',
+        scroller:'body',
+        start:'top 60%',
+        end:'top 60%',
+        scrub:2
+    }
+})
+var tl2=gsap.timeline()
+tl2.from('.skill-set .title h2',{
+    opacity:0,
+    y:-50,
+    scrollTrigger:{
+        trigger:'.skill-set .title h2',
+        scroller:'body',
+        start:'top 60%',
+        end:'top 60%',
+        scrub:5
+    }
+})
+tl2.from('.skill-set .container .skill img',{
+    opacity:0,
+    scale:0.5,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:'.skill-set .container .skill img',
+        scroller:'body',
+        start:'top 60%',
+        end:'top 60%',
+        scrub:2
+    }
+})
+
